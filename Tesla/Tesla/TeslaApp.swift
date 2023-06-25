a13e18eb545ffb9cc6100294222cb862e57995e3
Marc Dos Santos <mdossantosdev@gmail.com>
2023-06-25 14:41:15 +0200

    Init Tesla App

---
+++ Tesla/Tesla/TeslaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaApp.swift
+//  Tesla
+//
+//  Created by Marc on 25/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TeslaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
