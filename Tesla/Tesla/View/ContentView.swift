ecfcb960a2c917d5164c624fbc3b86fee6608397
Marc Dos Santos <mdossantosdev@gmail.com>
2023-06-25 17:00:57 +0200

    Add HomeHeader

--- Tesla/Tesla/View/ContentView.swift
+++ Tesla/Tesla/View/ContentView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
+            ScrollView {
+                HomeHeader()
+                    .padding()
+            }
+            
             VoiceCommandButton()
         }
     }
---
+++ Tesla/Tesla/View/HomeHeader.swift
@@ -0,0 +1,40 @@
+//
+//  HomeHeader.swift
+//  Tesla
+//
+//  Created by Marc on 25/06/2023.
+//
+
+import SwiftUI
+
+struct HomeHeader: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Model 3".uppercased())
+                    .font(.caption2)
+                    .fontWeight(.medium)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 4)
+                    .foregroundColor(Color.white)
+                    .background(Color.red)
+                    .clipShape(Capsule())
+                
+                Text("Mach Five")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.black)
+            }
+            
+            Spacer()
+            
+            HStack {}
+        }
+    }
+}
+
+struct HomeHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeHeader()
+    }
+}
